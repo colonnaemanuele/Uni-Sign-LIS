41587fd0db2564c4f77fe2f744d041905864b359
Blanco <79834454+blanco003@users.noreply.github.com>
2025-06-13 08:30:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,27 +3,27 @@ mt5_path = "./pretrained_weight/mt5-base"
 # label paths
 train_label_paths = {
                     "LIS": "/content/drive/MyDrive/LIS3/data/LIS_Labels.json",
-                    "LIS_TEST": "/content/drive/MyDrive/LIS2_TEST/data/LIS_Labels.json"
+                    "LIS_TEST": "/content/drive/MyDrive/LIS3_TEST/data/LIS_Test_Labels.json"
                     }
 
 dev_label_paths = {
                     "LIS": "/content/drive/MyDrive/LIS3/data/LIS_Labels.json",
-                    "LIS_TEST": "/content/drive/MyDrive/LIS2_TEST/data/LIS_Labels.json"
+                    "LIS_TEST": "/content/drive/MyDrive/LIS3_TEST/data/LIS_Test_Labels.json"
                     }
 
 test_label_paths = {
                     "LIS": "/content/drive/MyDrive/LIS3/data/LIS_Labels.json",
-                    "LIS_TEST": "/content/drive/MyDrive/LIS2_TEST/data/LIS_Labels.json"
+                    "LIS_TEST": "/content/drive/MyDrive/LIS3_TEST/data/LIS_Test_Labels.json"
                     }
 
 # video paths
 rgb_dirs = {
             "LIS": "/content/drive/MyDrive/LIS3/rgb_format_resized",
-            "LIS_TEST": "/content/drive/MyDrive/LIS2_TEST/rgb_format"
+            "LIS_TEST": "/content/drive/MyDrive/LIS3_TEST/rgb_format_resized"
             }
 
 # pose paths
 pose_dirs = {
             "LIS": "/content/drive/MyDrive/LIS3/pose_format",
-            "LIS_TEST": "/content/drive/MyDrive/LIS2_TEST/pose_format"
+            "LIS_TEST": "/content/drive/MyDrive/LIS3_TEST/pose_format"
             }
